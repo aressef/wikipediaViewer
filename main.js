238efd982fdf09be05f1c9b4f2fbe515befee0e0
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-06-17 17:47:14 -0400

    Searched string now calls api

--- main.js
+++ main.js
@@ -9,18 +9,20 @@ function keyup(e) {
     var replaceSpaces = inputTextValue.replace(' ', '%20');
     console.log(replaceSpaces);
     url = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch=" + replaceSpaces + "&utf8=";
-    xhttp.send();
+    getRequest();
   }
 }
 
-// AJAX Request to WikiMedia
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if(this.readyState == 4 && this.status == 200) {
-    console.log(this);
+// AJAX Request to MediaWiki API
+function getRequest() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      console.log(this);
+    }
   }
+  xhttp.open("GET", url, true);
+  xhttp.send();
 }
-xhttp.open("GET", url, true);
-//xhttp.send();
 
 // https://en.wikipedia.org/w/api.php?action=query&format=jsonfm&list=search&origin=*&errorformat=plaintext&srsearch=Albert%20Einstein&utf8=
