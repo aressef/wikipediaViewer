8df6b03fa18ccd92b9949eab283767c9d755e7b2
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-06-18 15:52:05 -0400

    Add top 10 search results from wiki api

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ function keyup(e) {
   inputTextValue = e.target.value;
 
   if(e.keyCode == 13) {
-    var replaceSpaces = inputTextValue.replace(' ', '%20');
-    console.log(replaceSpaces);
+    var replaceSpaces = encodeURIComponent(inputTextValue.replace(' ', '%20'));
     url = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch=" + replaceSpaces + "&utf8=";
     getRequest();
   }
@@ -18,11 +17,15 @@ function getRequest() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
-      console.log(this);
+      var parsed = JSON.parse(this.responseText);
+      console.log(parsed);
+      console.log(parsed.query.search[0]);
+
+      for(var i = 0; i < 10; i++) {
+        console.log(parsed.query.search[i]);
+      }
     }
   }
   xhttp.open("GET", url, true);
   xhttp.send();
 }
-
-// https://en.wikipedia.org/w/api.php?action=query&format=jsonfm&list=search&origin=*&errorformat=plaintext&srsearch=Albert%20Einstein&utf8=
