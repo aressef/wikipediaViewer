57e74cf2ba1468e38bd8ea8e5364f57ec5d68db5
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-06-19 20:55:51 -0400

    Add searchResultSnippet without html tags

--- main.js
+++ main.js
@@ -19,13 +19,40 @@ function getRequest() {
     if(this.readyState == 4 && this.status == 200) {
       var parsed = JSON.parse(this.responseText);
       console.log(parsed);
-      console.log(parsed.query.search[0]);
+      parsedResults = parsed.query.search;
 
       for(var i = 0; i < 10; i++) {
         console.log(parsed.query.search[i]);
       }
+      addResults();
     }
   }
   xhttp.open("GET", url, true);
   xhttp.send();
+}
+
+function addResults() {
+  var resultList = document.querySelector('ul');
+
+  for (var i = 0; i < 10; i++) {
+    var searchResultsDiv = document.createElement('div');
+    searchResultsDiv.className = 'searchResultsDiv';
+    var searchResults = document.createElement('ul');
+    var searchResultTitle = document.createElement('li');
+    var searchResultSnippet = document.createElement('li');
+
+    searchResultTitle.textContent = parsedResults[i].title;
+    searchResultSnippet.textContent = parsedResults[i].snippet;
+
+    // Remove HTML tags from snippet
+    var snippetText = searchResultSnippet;
+    var strippedString = snippetText.textContent.replace(/(<([^>]+)>)/ig,"");
+    searchResultSnippet.innerHTML = strippedString;
+
+    resultList.appendChild(searchResultsDiv);
+    searchResultsDiv.appendChild(searchResults);
+    searchResults.appendChild(searchResultTitle);
+    searchResults.appendChild(searchResultSnippet);
+  }
+
 }
